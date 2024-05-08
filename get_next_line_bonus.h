30ee1b410043e5c3b343502cf5b008be2ce360e1
Bruno Miguel Fernandes Tender <brfernan@c2r8s3.42lisboa.com>
2024-05-08 19:08:46 +0100

    backup

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 17:35:17 by brfernan          #+#    #+#             */
+/*   Updated: 2023/11/17 18:56:12 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buf);
+int		buffer(char	*buf);
+char	*clearbuf(char *buf);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 17:35:23 by brfernan          #+#    #+#             */
+/*   Updated: 2023/11/27 13:23:04 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *line, char *buf)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = malloc(ft_strlen(line) + ft_strlen(buf) + 1);
+	if (!str)
+		return (free (line), NULL);
+	while (line && line[i])
+	{
+		str[i] = line[i];
+		i++;
+	}
+	while (buf && buf[j])
+	{
+		str[i++] = buf[j];
+		if (buf[j] == '\n')
+			break ;
+		j++;
+	}
+	str[i] = 0;
+	free (line);
+	return (str);
+}
